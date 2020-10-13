cd0c7a1ca22d30de16d93a6fd5f02305bbf91327
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-10-14 03:05:06 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fighter> troopOne = new List<Fighter>();
+            List<Fighter> troopTwo = new List<Fighter>();
+
+            AddTroopOne(troopOne);
+            AddTroopTwo(troopTwo);
+
+            var transferFighters = troopOne.Where(fighter => fighter.Name.StartsWith("Б")).ToList();
+
+
+            for (int i = 0; i < transferFighters.Count; i++)
+            {
+                troopTwo.Add(transferFighters[i]);
+            }
+
+            for (int i = 0; i < troopTwo.Count; i++)
+            {
+                Console.WriteLine($"имя бойца - {troopTwo[i].Name}");
+            }
+            
+            Console.ReadKey();
+        }
+
+        private static void AddTroopOne(List<Fighter> troop)
+        {
+            troop.Add(new Fighter("Бургузинов Антон"));
+            troop.Add(new Fighter("Антонов Игорь"));
+            troop.Add(new Fighter("Белов Андрей"));
+            troop.Add(new Fighter("Греков Виталий"));
+            troop.Add(new Fighter("Васильев Василий"));
+        }
+        private static void AddTroopTwo(List<Fighter> troop)
+        {
+            troop.Add(new Fighter("Битонов Александр"));
+            troop.Add(new Fighter("Болегов Николай"));
+            
+        }
+    }    
+    class Fighter
+    {
+        public string Name { get; private set; }
+        public Fighter(string name)
+        {
+            Name = name;
+        }
+    }
+}
